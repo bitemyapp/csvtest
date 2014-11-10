4a5ee106dd78b1180a03d0d155fa0d00a3f4676e
Chris Allen <cma@bitemyapp.com>
2014-11-09 22:05:21 -0600

    edits

--- Main.hs
+++ Main.hs
@@ -1,19 +1,18 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString as BS
-import qualified Data.Vector as V
+-- import qualified Data.ByteString as BS
+-- import qualified Data.Vector as V
 import qualified Data.Foldable as F
-import Data.Conduit.List as CL
-import Data.Text (Text)
-import Control.Monad.IO.Class (MonadIO, liftIO)
+-- import Data.Conduit.List as CL
+-- import Data.Text (Text)
+-- import Control.Monad.IO.Class (MonadIO, liftIO)
 
 -- cassava
 -- import Data.Csv
 
 -- cassava streaming
--- import Data.Csv.Streaming
+import Data.Csv.Streaming
 
 -- csv-conduit
 -- import Data.Conduit
@@ -21,60 +20,71 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 -- import Data.CSV.Conduit
 
 -- pipes-csv
-import Data.Csv (HasHeader(..), (.:), FromNamedRecord(..), Record)
-import Pipes.Csv (decode, decodeByName)
-import Pipes.ByteString (fromLazy)
-import Pipes
-import qualified Pipes.Prelude as P
-import Data.Either (isRight, either)
-import Control.Monad (forever)
+-- import Data.Csv (HasHeader(..), (.:), FromNamedRecord(..), Record)
+-- import Pipes.Csv (decode, decodeByName)
+-- import Pipes.ByteString (fromLazy)
+-- import Pipes
+-- import qualified Pipes.Prelude as P
+-- import Data.Either (isRight, either)
+-- import Control.Monad (forever)
 
 -- "igarary01",2010,"NYN",34
 type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
 
+fourth :: (a, b, c, d) -> d
+fourth (_, _, _, d) = d
+
+baseballStats :: BL.ByteString -> Records BaseballStats
+baseballStats = decode NoHeader
+
 -- cassava with foldr, ~40mb
+-- main :: IO ()
 -- main = do
 --   csvData <- BL.readFile "batting.csv"
---   let v = decode NoHeader csvData :: Either String (V.Vector BaseballStats)
---   let summed = fmap (V.foldr summer 0) v
+--   let summed = fmap (V.foldr summer 0) (baseballStats csvData)
 --   putStrLn $ "Total atBats was: " ++ (show summed)
---   where summer = \(name, year :: Int, team, atBats :: Int) sum -> sum + atBats
+--   where summer = (+) . fourth
+
+folder :: F.Foldable f => f (a, b, c, Int) -> Int
+folder = F.foldr summer 0
+
+summer :: (a, b, c, Int) -> Int -> Int
+summer = (+) . fourth
 
 -- cassava with streaming, ~8-10MB
--- main = do
---   csvData <- BL.readFile "batting.csv"
---   let v = decode NoHeader csvData :: Records BaseballStats
---   let summed = F.foldr summer 0 v
---   putStrLn $ "Total atBats was: " ++ (show summed)
---   where summer = \(name, year :: Int, team, atBats :: Int) sum -> sum + atBats
+main :: IO ()
+main = do
+  csvData <- BL.readFile "batting.csv"
+  let summed = F.foldr summer 0 (baseballStats csvData)
+  putStrLn $ "Total atBats was: " ++ (show summed)
 
 -- pipes-csv
 
-battingData :: Monad m =>
-               Producer BS.ByteString m ()
-               -> Producer (Either String BaseballStats) m ()
-battingData = decode NoHeader
+-- battingData :: Monad m =>
+--                Producer BS.ByteString m ()
+--                -> Producer (Either String BaseballStats) m ()
+-- battingData = decode NoHeader
 
-battingSource :: Monad m => IO (Producer BS.ByteString m ())
-battingSource = fmap fromLazy (BL.readFile "batting.csv")
+-- battingSource :: Monad m => IO (Producer BS.ByteString m ())
+-- battingSource = fmap fromLazy (BL.readFile "batting.csv")
 
 -- dropLeft = forever $ await >>= (\x -> case x of
 --                                    Left _  -> return ()
 --                                    Right x -> yield x)
 
-foldE :: Either String BaseballStats -> Int
-foldE = either (const 0) (\(_, _, _, i) -> i)
+-- foldE :: Either String BaseballStats -> Int
+-- foldE = either (const 0) (\(_, _, _, i) -> i)
 
-main :: IO ()
-main = do
-  src <- battingSource :: IO (Producer BS.ByteString IO ())
+-- main :: IO ()
+-- main = do
+  -- src <- battingSource :: IO (Producer BS.ByteString IO ())
   --  >-> (lift . print)
   -- (P.filter isRight)
   -- let blah = sumAtBats (P.map fmap (battingData src))
-  P.sum $ battingData src >-> P.map foldE >>= print
+  -- P.sum (battingData src >-> P.map foldE) >>= print
   -- >-> P.fold (fmap (\n x -> n + x)) 0 id
   -- runEffect $ for (battingData src) (lift . print)
-  return ()
+  -- return ()
 
 -- tombstoned csv-conduit
 -- instance BL.ByteString BaseballStats where  
